e816bb66cddc7f8a52bd763b6779f55292ae5ef6
Doak Covington <doakcovington@protonmail.com>
2020-02-24 04:42:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     #end
-    binding.pry
+    #binding.pry
   end
 
   #Use CSS selector to grab all of the HTML elements that contain a course
