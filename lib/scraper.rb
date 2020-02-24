ff45a35c7cd3ea0a87f2df493e49d8ec389811c6
Doak Covington <doakcovington@protonmail.com>
2020-02-24 04:24:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
 
   #use Nokogiri and open-uri to grab the entire HTML document from web page
   def get_page
-
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   #Use CSS selector to grab all of the HTML elements that contain a course
